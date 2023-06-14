3671f045b162f72e568032e926dc261200493059
12RUP <110966987+12RUP@users.noreply.github.com>
2023-06-14 16:48:46 +0300

    Add files via upload

---
+++ основа/Витрина/Витрина.js
@@ -0,0 +1,36 @@
+const slides =document.querySelectorAll('.slide')
+
+for (const slide of slides){
+    slide.addEventListener('click',function(){
+        clearactiveClass()
+        slide.classList.add('active')
+        
+    })
+}
+
+function clearactiveClass(){
+    slides.forEach(function(slide){
+        slide.classList.remove('active')
+    })
+}
+
+let watch=document.querySelector('.watch')
+watch.addEventListener('click',function(){
+    window.open('Переходы/ep.html')
+})
+let watch2=document.querySelector('.watch2')
+let watch3=document.querySelector('.watch3')
+let watch4=document.querySelector('.watch4')
+let watch5=document.querySelector('.watch5')
+watch2.addEventListener('click',function(){
+    window.open('Переходы/e2p.html')
+})
+watch3.addEventListener('click',function(){
+    window.open('Переходы/ep3.html')
+})
+watch4.addEventListener('click',function(){
+    window.open('Переходы/ep4.html')
+})
+watch5.addEventListener('click', function(){
+    window.open('Переходы/upload/index.html')
+} )
